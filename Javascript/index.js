48f0811ceaeb7d7b36638eb7b88dfa6b6d642c24
Abiral Sharma <abiralsharma31@gmail.com>
2023-10-22 14:16:55 +0545

    fix:try to make the site dynamic

--- Javascript/index.js
+++ Javascript/index.js
@@ -5,9 +5,25 @@ fetch('./Json/data.json')
     }
 else
 return reponse.json();
-}).then((myobj)=>{
+})
+.then(myobj=>{
 const mydata=myobj;
 console.log(mydata);
-}).catch((err)=>{
-    console.log(err);
+const weatherSearch=document.createElement("div");
+ weatherSearch.className="weather";
+ weatherSearch.innerHTML=`
+ <form id="search-weather">
+ <input type="text" placeholder="Enter the name of country" id="option"> 
+ <button id="button"><i class="fa-solid fa-magnifying-glass"></i></button> 
+ </form>`
+ document.body.appendChild(weatherSearch);
+ mydata.forEach(data=> {
+ console.log(data);
+ const  countryWeather=document.createElement("div");
+ countryWeather.className="weatherInfo";
+ countryWeather.innerHTML=data.image;
+ weatherSearch.appendChild(countryWeather);
+ })})
+ .catch(err=>{
+ console.log(err);
 })
